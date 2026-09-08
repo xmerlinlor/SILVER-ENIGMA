0ef22226bbd6a2fc158632ae1a3a58756df20854
xmerlinlor <xmerlinlordbot@gmail.com>
2026-09-08 05:45:12 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+  BOT_NAME: process.env.BOT_NAME || "SILVER-ENIGMA",
+  PREFIX: process.env.PREFIX || ".",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+  PORT: process.env.PORT || 3000
+};
